7e07fb714df14e56ee40f805193047addddc74e7
dlalfo1 <mirae1213@daum.net>
2023-04-10 17:33:46 +0900

    2023-04-10

---
+++ 01_IoC_Container/src/main/java/com/gdu/app01/xml02/Address.java
@@ -0,0 +1,31 @@
+package com.gdu.app01.xml02;
+
+public class Address {
+
+	private String doronameAddress;
+	private String jibunAddress;
+	private Contact contact;
+	
+	public String getDoronameAddress() {
+		return doronameAddress;
+	}
+	public void setDoronameAddress(String doronameAddress) {
+		this.doronameAddress = doronameAddress;
+	}
+	public String getJibunAddress() {
+		return jibunAddress;
+	}
+	public void setJibunAddress(String jibunAddress) {
+		this.jibunAddress = jibunAddress;
+	}
+	public Contact getContact() {
+		return contact;
+	}
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+	
+
+	
+	
+}
---
+++ 01_IoC_Container/src/main/java/com/gdu/app01/xml02/Contact.java
@@ -0,0 +1,21 @@
+package com.gdu.app01.xml02;
+
+public class Contact {
+
+	private String callNo;
+	private String faxNo;
+	
+	public String getCallNo() {
+		return callNo;
+	}
+	public void setCallNo(String callNo) {
+		this.callNo = callNo;
+	}
+	public String getFaxNo() {
+		return faxNo;
+	}
+	public void setFaxNo(String faxNo) {
+		this.faxNo = faxNo;
+	}
+	
+}
